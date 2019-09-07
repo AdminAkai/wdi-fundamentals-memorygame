7d7bcb55762eaa1939a227d1f5ecfa65bcc35446
JCaptainnnn <49946613+JCaptainnnn@users.noreply.github.com>
2019-09-07 02:45:17 -0400

    latest memory game build: 0.2, added game logic

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var cardOne;
+var cardTwo;
+var cardThree;
+var cardFour;
+
+cardOne = "king";
+cardTwo = "king";
+cardThree = "queen";
+cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
