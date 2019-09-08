c198a79debf3643b81bc908cc7089298db70901d
JCaptainnnn <49946613+JCaptainnnn@users.noreply.github.com>
2019-09-08 04:44:37 -0400

    latest memory game build: 0.3, added more game logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-var cardOne;
-var cardTwo;
-var cardThree;
-var cardFour;
+const cards = ['king', 'king', 'queen', 'queen'];
+const cardsInPlay = [];
 
-cardOne = "king";
-cardTwo = "king";
-cardThree = "queen";
-cardFour = "queen";
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
