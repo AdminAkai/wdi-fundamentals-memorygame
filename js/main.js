b0c19a434e08ff436cef22a38a45cf5d8b6da323
JCaptainnnn <49946613+JCaptainnnn@users.noreply.github.com>
2019-09-09 06:15:47 -0400

    latest memory game build 1.4, added point system, expanded amount of cards

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ const cards = [
 ];
 
 const cardsInPlay = [];
+var totalpoints = 0;
 
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -40,12 +41,20 @@ function checkForMatch() {
     if (cardsInPlay.length === 2) {
         if (cardsInPlay[0] === cardsInPlay[1]) {
             alert("You found a match!");
+            totalpoints += 50;
+            showPoints();
             resetCardsInPlay();
         } else {
             alert("Sorry, try again.");
+            showPoints();
             resetCardsInPlay();
         }
     }
+}
+
+function showPoints() {
+    const currentPoints = document.getElementById('totalpoints');
+    currentPoints.innerHTML = totalpoints;
 }
 
 function flipCard() {
@@ -60,13 +69,15 @@ function flipCard() {
 
 function createBoard() {
     shuffle(cards);
-    for (var i = 0; i < cards.length; i++) {
-        var cardElement = document.createElement('img');
-        cardElement.className = "cards";
-        cardElement.setAttribute('src', 'images/back.png');
-        cardElement.setAttribute('data-id', i);
-        cardElement.addEventListener('click', flipCard);
-        document.getElementById('game-board').appendChild(cardElement);
+    for (var x = 0; x < 3; x++) {
+        for (var i = 0; i < cards.length; i++) {
+            var cardElement = document.createElement('img');
+            cardElement.className = "cards";
+            cardElement.setAttribute('src', 'images/back.png');
+            cardElement.setAttribute('data-id', i);
+            cardElement.addEventListener('click', flipCard);
+            document.getElementById('game-board').appendChild(cardElement);
+        }
     }
 }
 
@@ -76,6 +87,7 @@ function resetBoard() {
         removeCards.removeChild(removeCards.firstChild);
         cardsInPlay.pop();
     }
+    totalpoints = 0;
     createBoard();
 }
 
